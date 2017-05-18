c5aa070074062cea4302f4e970a82ef2939977cc
John Balke <john.balke@live.com.au>
2017-05-18 16:04:52 +0100

    Added support of heroku dyno metadata.

--- app/config/auth.js
+++ app/config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
   twitterAuth: {
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: `${process.env.APP_URL}auth/twitter/callback`,
+    callbackURL: `${process.env.APP_URL || 'http://' + process.env.HEROKU_APP_NAME}/auth/twitter/callback`,
   },
 };
